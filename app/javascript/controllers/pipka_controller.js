de5b347089558f406278fcc432d9885ec257c45b
KVexcavator <str_token@mail.ru>
2020-08-08 19:22:50 +0300

    One target to rule them all

--- app/javascript/controllers/pipka_controller.js
+++ app/javascript/controllers/pipka_controller.js
@@ -3,7 +3,8 @@ import { Controller } from 'stimulus';
 export default class extends Controller {
   static targets = ["output"]
 
-  say_hello(){
-    this.outputTarget.textContent = "Hello Pipka and Stimulus!"
+  function_add(){
+    for(var index in this.outputTargets)
+      this.outputTargets[index].textContent = "And Pipla yet"
   }
 }
